cf9fec62ccddf5065c77d38023dc8982ea3f295e
Nguyen Thi Thu Hien <hien.ntt@ad.tctav.com>
2019-12-02 14:41:30 +0700

    function node js

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 
 const _ = require("lodash");
 
-console.log(_.chunk(['a', 'b', 'c', 'd'], 3))
+var arr = ['a','b','c','d'];
 
+console.log(_.chunk(arr, 3))
+
+console.log(arr[3])
+console.log(arr.length)
+arr.push("e")
+console.log(arr.length)
+
+function hello() {
+    console.log("hello world");
+}
+
+function callFunc(fn) {
+    fn()
+}
+
+callFunc(hello)
+
+var str = function(){
+    console.log("Hello world!")
+}
+
+str();
