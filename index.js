b45b91f34ad0a243dc0cce6904ea7cceecf7a495
Nguyen Thi Thu Hien <hien.ntt@ad.tctav.com>
2019-12-02 11:35:59 +0700

    sencond commit

--- index.js
+++ index.js
@@ -2,11 +2,4 @@ console.log("hello world")
 
 const _ = require("lodash");
 
-
-_.chunk(['a', 'b', 'c', 'd'], 2);
-// => [['a', 'b'], ['c', 'd']]
- 
-
-// => [['a', 'b', 'c'], ['d']]
-
 console.log(_.chunk(['a', 'b', 'c', 'd'], 3))
